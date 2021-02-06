df4a45c786b428b8197337ff09a856b8fb0402e6
exTerEX <developer@sagen.io>
2021-02-06 02:59:42 +0100

    Exported settings from setup.cfg to setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,19 @@ Distutils setup file, used to install or test 'pendulum'
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup()
+    setup(
+        name="pendulum",
+        version="0.1.0",
+        author="Andreas Sagen",
+        license="AFL-3.0",
+        install_requires=[
+            "numpy==1.20.0",
+            "scipy==1.6.0",
+            "matplotlib==3.3.4"
+        ],
+        zip_safe=False,
+        extras_require={
+            "test": ["pytest==6.2.2"]
+        },
+        packages=["source"]
+    )
